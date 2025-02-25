61469bc4113e05a3df91ab3b172934cad9b2dd0b
wuhao10 <wuhao10@xiaomi.com>
2025-02-25 21:51:33 +0800

    DefaultSqlSessionFactory 添加中文注释

--- src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSessionFactory.java
+++ src/main/java/org/apache/ibatis/session/defaults/DefaultSqlSessionFactory.java
@@ -32,63 +32,134 @@ import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.managed.ManagedTransactionFactory;
 
 /**
- * @author Clinton Begin
+ * DefaultSqlSessionFactory 是 SqlSessionFactory 的默认实现。
+ * 它负责创建 SqlSession 实例，管理数据库连接和事务。
+ *
+ * @作者 Clinton Begin
  */
 public class DefaultSqlSessionFactory implements SqlSessionFactory {
 
+  // 配置对象，包含了MyBatis的所有配置信息
   private final Configuration configuration;
 
+  /**
+   * 构造函数，接受一个 Configuration 对象。
+   *
+   * @param configuration MyBatis 配置对象
+   */
   public DefaultSqlSessionFactory(Configuration configuration) {
     this.configuration = configuration;
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用默认的执行器类型和不自动提交。
+   *
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession() {
     return openSessionFromDataSource(configuration.getDefaultExecutorType(), null, false);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用默认的执行器类型和指定的自动提交设置。
+   *
+   * @param autoCommit 是否自动提交
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(boolean autoCommit) {
     return openSessionFromDataSource(configuration.getDefaultExecutorType(), null, autoCommit);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用指定的执行器类型和不自动提交。
+   *
+   * @param execType 执行器类型
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(ExecutorType execType) {
     return openSessionFromDataSource(execType, null, false);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用默认的执行器类型和指定的事务隔离级别。
+   *
+   * @param level 事务隔离级别
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(TransactionIsolationLevel level) {
     return openSessionFromDataSource(configuration.getDefaultExecutorType(), level, false);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用指定的执行器类型和事务隔离级别。
+   *
+   * @param execType 执行器类型
+   * @param level 事务隔离级别
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(ExecutorType execType, TransactionIsolationLevel level) {
     return openSessionFromDataSource(execType, level, false);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用指定的执行器类型和自动提交设置。
+   *
+   * @param execType 执行器类型
+   * @param autoCommit 是否自动提交
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(ExecutorType execType, boolean autoCommit) {
     return openSessionFromDataSource(execType, null, autoCommit);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用默认的执行器类型和指定的数据库连接。
+   *
+   * @param connection 数据库连接
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(Connection connection) {
     return openSessionFromConnection(configuration.getDefaultExecutorType(), connection);
   }
 
+  /**
+   * 打开一个新的 SqlSession，使用指定的执行器类型和数据库连接。
+   *
+   * @param execType 执行器类型
+   * @param connection 数据库连接
+   * @return 新的 SqlSession 实例
+   */
   @Override
   public SqlSession openSession(ExecutorType execType, Connection connection) {
     return openSessionFromConnection(execType, connection);
   }
 
+  /**
+   * 获取当前的配置对象。
+   *
+   * @return 配置对象
+   */
   @Override
   public Configuration getConfiguration() {
     return configuration;
   }
-
+  /**
+   * 从数据源打开一个新的 SqlSession。
+   *
+   * @param execType 执行器类型
+   * @param level 事务隔离级别
+   * @param autoCommit 是否自动提交
+   * @return 新的 SqlSession 实例
+   */
   private SqlSession openSessionFromDataSource(ExecutorType execType, TransactionIsolationLevel level,
-      boolean autoCommit) {
+                                               boolean autoCommit) {
     Transaction tx = null;
     try {
       final Environment environment = configuration.getEnvironment();
@@ -97,21 +168,27 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory {
       final Executor executor = configuration.newExecutor(tx, execType);
       return new DefaultSqlSession(configuration, executor, autoCommit);
     } catch (Exception e) {
-      closeTransaction(tx); // may have fetched a connection so lets call close()
+      closeTransaction(tx); // 可能已经获取了连接，所以需要关闭
       throw ExceptionFactory.wrapException("Error opening session.  Cause: " + e, e);
     } finally {
       ErrorContext.instance().reset();
     }
   }
 
+  /**
+   * 从现有的数据库连接打开一个新的 SqlSession。
+   *
+   * @param execType 执行器类型
+   * @param connection 数据库连接
+   * @return 新的 SqlSession 实例
+   */
   private SqlSession openSessionFromConnection(ExecutorType execType, Connection connection) {
     try {
       boolean autoCommit;
       try {
         autoCommit = connection.getAutoCommit();
       } catch (SQLException e) {
-        // Failover to true, as most poor drivers
-        // or databases won't support transactions
+        // 如果驱动或数据库不支持事务，则自动提交
         autoCommit = true;
       }
       final Environment environment = configuration.getEnvironment();
@@ -126,6 +203,12 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory {
     }
   }
 
+  /**
+   * 从环境中获取事务工厂。
+   *
+   * @param environment 环境对象
+   * @return 事务工厂
+   */
   private TransactionFactory getTransactionFactoryFromEnvironment(Environment environment) {
     if (environment == null || environment.getTransactionFactory() == null) {
       return new ManagedTransactionFactory();
@@ -133,12 +216,17 @@ public class DefaultSqlSessionFactory implements SqlSessionFactory {
     return environment.getTransactionFactory();
   }
 
+  /**
+   * 关闭事务。
+   *
+   * @param tx 事务对象
+   */
   private void closeTransaction(Transaction tx) {
     if (tx != null) {
       try {
         tx.close();
       } catch (SQLException ignore) {
-        // Intentionally ignore. Prefer previous error.
+        // 有意忽略。优先处理之前的错误。
       }
     }
   }
